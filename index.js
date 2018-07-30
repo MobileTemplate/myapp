80729aa5b1a71877f66390627c2dc37396f0ef84
zhongyu <931564975@qq.com>
2018-07-30 18:11:58 +0800

    创建项目

---
+++ index.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+
+// import {AppRegistry,Component} from 'react-native';
+// import App from './app/App';
+// import {name as appName} from './app.json';
+
+// class Electricity extends Component {
+// 	render() {
+//     	return (
+// 			<App />
+//     	);
+// 	}
+// }
+
+// AppRegistry.registerComponent(appName, () => Electricity);
